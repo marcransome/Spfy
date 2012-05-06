13a04b78421a5c451fe495d445c105ccb333b578
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2012-05-06 17:42:36 +0100

    added missing license text and fixed minor url issue

--- lib/spfy/optionreader.rb
+++ lib/spfy/optionreader.rb
@@ -1,3 +1,24 @@
+#
+#  optionreader.rb
+#  Spfy ("spiffy")
+#
+#  Copyright (c) 2012, Marc Ransome <marc.ransome@fidgetbox.co.uk>
+#
+#  This file is part of Spfy.
+#
+#  Spfy is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Spfy is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Spfy.  If not, see <http://www.gnu.org/licenses/>.
+
 class OptionReader
 
 	def self.parse(args)
