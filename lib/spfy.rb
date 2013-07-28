dfa6a713b8d3abbed2af81098b4861aa25d32b67
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2013-07-28 17:32:13 +0100

    major refactoring and code simplification

--- lib/spfy.rb
+++ lib/spfy.rb
@@ -23,208 +23,115 @@ require "spfy/optionreader"
 require "optparse"
 require "ostruct"
 require "taglib"
-require 'find'
-require 'uri'
+require "find"
+require "uri"
 
-# The main Spfy class
 class Spfy
-	
-	VERSION = "0.2.2"
+  
+  VERSION = "1.0.0"
+  USAGE = "Use `#{File.basename($0)} --help` for available options."
+    
+  @xspf_tags = {
+    :header =>          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
+                        "<playlist version=\"1\" xmlns=\"http://xspf.org/ns/0/\">\n"\
+                        "\t<trackList>\n",
+    :footer =>          "\t</trackList>\n</playlist>\n",              
+    :title_start =>     "\t\t\t<title>",
+    :title_end =>       "</title>\n",
+    :creator_start =>   "\t\t\t<creator>",
+    :creator_end =>     "</creator>\n",
+    :album_start =>     "\t\t\t<album>",
+    :album_end =>       "</album>\n",
+    :location_start =>  "\t\t\t<location>file://",
+    :location_end =>    "</location>\n",
+    :track_start =>     "\t\t<track>\n",
+    :track_end =>       "\t\t</track>\n",
+    :track_num_start => "\t\t\t<trackNum>",
+    :track_num_end =>   "</trackNum>\n"
+  }
+  
+  def self.parse_args
+    begin
+      # test for zero arguments
+      if ARGV.empty? then
+        self.exit_with_banner
+      end
+      
+      # parse command-line arguments
+      @options = OptionReader.parse(ARGV)
+      
+      # test for zero source paths
+      if @options.dirs.empty?
+        exit_with_message("No source path(s) specified.")
+      end
+      
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument => error
+      exit_with_message(error.to_s.capitalize)
+    end
+  end
+  
+  def self.generate_xml
 
-	##
-	# Starts the XSPF generator.
-	#
-	def self.generate
+    if @options.output.any?
+      puts "Generating XML..."
+      self.capture_stdout
+    end
+    
+    puts @xspf_tags[:header]
+    tracks_processed = 0
+    
+    @options.dirs.each do |dir|
+      begin
+        Find.find(dir) do |path|
+          TagLib::FileRef.open(path) do |fileref|  
+            tag = fileref.tag
+            
+            next if tag.nil? # skip files with no tags
+            
+            puts "#{@xspf_tags[:track_start]}"
+            
+            if !@options.hide_location
+              encoded_path = URI.escape(path).sub("%5C", "/") # percent encode string for local path
+              puts "#{@xspf_tags[:location_start]}#{encoded_path}#{@xspf_tags[:location_end]}"
+            end
+            
+            puts "#{@xspf_tags[:title_start]}#{tag.title}#{@xspf_tags[:title_end]}" if !@options.hide_title and !tag.title.nil?
+            puts "#{@xspf_tags[:creator_start]}#{tag.artist}#{@xspf_tags[:creator_end]}" if !@options.hide_artist and !tag.artist.nil?
+            puts "#{@xspf_tags[:album_start]}#{tag.album}#{@xspf_tags[:album_end]}" if !@options.hide_album and !tag.album.nil?
+            
+            if !@options.hide_tracknum and !tag.track.nil?
+              if tag.track > 0
+                puts "#{@xspf_tags[:track_num_start]}#{tag.track}#{@xspf_tags[:track_num_end]}"
+              end
+            end
+            
+            puts "#{@xspf_tags[:track_end]}"
+            
+            tracks_processed += 1
+            break if @options.tracks_to_process[0].to_i > 0 and tracks_processed == options.tracks_to_process[0].to_i
+          end
+        end
+      rescue Interrupt
+        abort("\nCancelled, exiting..")
+      end
+    end
+    puts @xspf_tags[:footer]
+    
+    $stdout = STDOUT if @options.output.any?
+  end
 
-		# start processing command line arguments
-		begin
-		
-			# short usage banner
-			simple_usage = "Use `#{File.basename($0)} --help` for available options."
-		
-			# test for zero arguments
-			if ARGV.empty? then
-				puts simple_usage
-				exit
-			end
-			
-			# parse command-line arguments
-			options = OptionReader.parse(ARGV)
-			
-			# dirs for traversing
-			dirs = options.dirs
-			
-			# test for zero source paths
-			if dirs.empty?
-				puts "No source path specified."
-				puts simple_usage
-				exit
-			end
-			
-		rescue OptionParser::InvalidOption => t
-			puts t
-			puts simple_usage
-			exit
-		rescue OptionParser::MissingArgument => m
-			puts m
-			puts simple_usage
-			exit
-		end
-		
-		# start processing source paths
-		if options.output.any?
-			# source path(s) provided, output should be to disk
-			
-			# open output file for writing
-			begin
-				xmlFile = File.open(options.output[0], "w")
-			rescue
-				puts "Unable to open output file for writing."
-				puts simple_usage
-				exit
-			end
-			
-			print "Generating XML.."
-
-			# write XSPF header
-			xmlFile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-			xmlFile.write("<playlist version=\"1\" xmlns=\"http://xspf.org/ns/0/\">\n")
-			xmlFile.write("\t<trackList>\n")
-			
-			# track count for track limit option
-			tracks_processed = 0
-			
-			# repeat for each source dir argument
-			dirs.each do |dir|
-	
-				begin
-					
-					# repeat for each file recursively
-					Find.find(dir) do |path|
-					
-						TagLib::FileRef.open(path) do |fileref|
-							
-							tag = fileref.tag
-							
-							# skip files with no tags
-							next if tag.nil?
-							
-							# write track metadata
-							xmlFile.write("\t\t<track>\n")
-							
-							if !options.hide_location
-								# generate a percent encoded string from the local path
-								encoded_path = URI.escape(path).sub("%5C", "/")
-								xmlFile.write("\t\t\t<location>file://#{encoded_path}</location>\n")
-							end
-							
-							xmlFile.write("\t\t\t<title>#{tag.title}</title>\n") if !options.hide_title and !tag.title.nil?
-							xmlFile.write("\t\t\t<creator>#{tag.artist}</creator>\n") if !options.hide_artist and !tag.artist.nil?
-							xmlFile.write("\t\t\t<album>#{tag.album}</album>\n") if !options.hide_album and !tag.album.nil?
-							
-							if !options.hide_tracknum and !tag.track.nil?
-								if tag.track > 0
-									xmlFile.write("\t\t\t<trackNum>#{tag.track}</trackNum>\n")
-								end
-							end
-							
-							xmlFile.write("\t\t</track>\n")
-							
-							# increment our track processed count
-							tracks_processed += 1
-							
-							# if a maximum number track numbe has been set, test whether we have reached the limit
-							if options.tracks_to_process[0].to_i > 0 and tracks_processed == options.tracks_to_process[0].to_i 
-								# write XSPF footer
-								xmlFile.write("\t</trackList>\n")
-								xmlFile.write("</playlist>\n")
-								xmlFile.close
-								print " success\n"
-								exit
-							end
-						end
-					end
-				rescue Interrupt
-					abort("\nCancelled, exiting..")
-				end # begin
-			end # dirs.each do |dir|
-			
-			# write XSPF footer
-			xmlFile.write("\t</trackList>\n")
-			xmlFile.write("</playlist>\n")
-			
-			xmlFile.close
-			
-			print " success\n"
-			
-		else
-			# no source path(s) provided, output to stdout
-			
-			# write XSPF header
-			puts "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-			puts "<playlist version=\"1\" xmlns=\"http://xspf.org/ns/0/\">\n"
-			puts "\t<trackList>\n"
-			
-			# track count for track limit option
-			tracks_processed = 0
-			
-			# repeat for each source dir argument
-			dirs.each do |dir|
-
-				begin
-				
-					# repeat for each file recursively
-					Find.find(dir) do |path|
-					
-						TagLib::FileRef.open(path) do |fileref|
-						
-							tag = fileref.tag
-		
-							# skip files with no tags
-							next if tag.nil?
-							
-							# output track metadata
-							puts "\t\t<track>\n"
-							
-							if !options.hide_location
-								encoded_path = URI.escape(path).sub("%5C", "/")
-								puts "\t\t\t<location>file://#{encoded_path}</location>\n"
-							end
-							
-							puts "\t\t\t<title>#{tag.title}</title>\n" if !options.hide_title and !tag.title.nil?
-							puts "\t\t\t<creator>#{tag.artist}</creator>\n" if !options.hide_artist and !tag.artist.nil?
-							puts "\t\t\t<album>#{tag.album}</album>\n" if !options.hide_album and !tag.album.nil?
-							
-							if !options.hide_tracknum and !tag.track.nil?
-								if tag.track > 0
-									puts "\t\t\t<trackNum>#{tag.track}</trackNum>\n"
-								end
-							end
-							
-							puts "\t\t</track>\n"
-							
-							# increment our track processed count
-							tracks_processed += 1
-
-							# if a maximum number track numbe has been set, test whether we have reached the limit
-							if options.tracks_to_process[0].to_i > 0 and tracks_processed == options.tracks_to_process[0].to_i 
-								# output XSPF footer
-								puts "\t</trackList>\n"
-								puts "</playlist>\n"
-								exit
-							end
-						end
-					end
-				rescue Interrupt
-					abort("\nCancelled, exiting..")
-				end # begin
-			end # dirs.each do |dir|
-			
-			# write XSPF footer
-			puts "\t</trackList>\n"
-			puts "</playlist>\n"
-		end
-
-	end # def self.generate
-
-end # class Spyf
+  def self.exit_with_message(message)
+    puts message if message
+    exit_with_banner
+  end
+  
+  def self.exit_with_banner
+    puts USAGE
+    exit
+  end
+    
+  def self.capture_stdout
+    $stdout = File.open(@options.output[0], "w")
+  end
+  
+end
