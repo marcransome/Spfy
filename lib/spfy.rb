4df5955b8a156947f82228122a81936c1fe4c162
iainb <iainspeed@gmail.com>
2018-03-13 14:45:56 +0000

    Improved the yard docs based on yardstick's pedantry.

--- lib/spfy.rb
+++ lib/spfy.rb
@@ -15,7 +15,10 @@ module Spfy
   # @see http://xspf.org/xspf-v1.html#rfc.section.4.1.1.2.14.1.1
   class Track
 
+    # The XSPF tags being targeted
     XSPF_TAGS = [:location, :album, :artist, :comment, :genre, :title, :trackNum, :year].freeze
+
+    # The translation of XSPF tags and Taglib tags
     XSPF_TO_TAGLIB = {
       :album    => :album,
       :artist   => :artist,
@@ -27,8 +30,11 @@ module Spfy
     }.freeze
 
 
-    # param [String,Pathname] path The location of the track.
-    # param [Hash] options
+    # @param [String,Pathname] path The location of the track.
+    # @param [Hash] options
+    # @api public
+    # @example
+    #   Spfy::Track.new(pn, options: @options)
     def initialize path, options: {}
       @path = Pathname(path).expand_path
       @template = Tilt::ERBTemplate.new(TEMPLATES.join("track.xml.erb"))
@@ -51,6 +57,8 @@ module Spfy
 
     # If there's a way to do dynamic delegation using Forwadable
     # I don't know what it is. Hence this.
+    # @api private
+    # @see https://ruby-doc.org/core-2.5.0/BasicObject.html#method-i-method_missing
     def method_missing(name, *args)
       if @available_tags.include? name.to_sym
         instance_eval <<-RUBY
@@ -66,7 +74,10 @@ module Spfy
 
 
     # Be a good person
-    def respond_to?(name, include_private = false)
+    # @api public
+    # @return [TrueClass]
+    # @see https://ruby-doc.org/core-2.5.0/Object.html#method-i-respond_to_missing-3F
+    def respond_to_missing?(name, *)
       if @available_tags.include? name.to_sym
         true
       else
@@ -76,6 +87,8 @@ module Spfy
 
 
     # Parse the options, mainly to find which ones were --no-
+    # @api private
+    # @return [<String>] Doesn't matter, it's held in the @noes instance var.
     def parse options
       @noes = options.each_with_object([]){|(k,v), obj|
                 if k =~ /^\-\-no\-/ and v
@@ -85,15 +98,28 @@ module Spfy
               }
     end
 
+
     # The file path
+    # @return [Pathname]
+    # @api public
     attr_reader :path
+
+
+    # The available tags after the options have been parsed
+    # @return [<String>]
+    # @api public
     attr_reader :available_tags
+
+
+    # A data object that holds the taglib data in XSPF format
+    # @return [Struct]
+    # @api semipublic
     attr_reader :data
 
 
-    # @private
     # Process the options into a track entity.
     # Calls TagLib
+    # @api private
     def process refresh=false
       if refresh or @processed == false
         TagLib::FileRef.open(@path.to_path) do |fileref|
@@ -114,6 +140,10 @@ module Spfy
 
     # For a block {|name,tag| ... }
     # @yield [name,tag] Yield the element name and its contents.
+    # @api public
+    # @example
+    #   # In ERB
+    #   <% each_tag do |name,tag| %><%= "<#{name}>#{tag}</#{name}>\n" %>
     def each_tag skip_nils=true
       return enum_for(:each_tag) unless block_given?
       @available_tags.each do |tag|
@@ -131,6 +161,7 @@ module Spfy
 
     # The renderer
     # @return [String]
+    # @api public
     def to_xml
       process
       @template.render(self)
@@ -140,8 +171,12 @@ module Spfy
 
   # Produces the playlist entity
   # @see http://xspf.org/xspf-v1.html#rfc.section.4.1.1
+  # @api public
   class Playlist
 
+    # @api public
+    # @example
+    #   playlist = Spfy::Playlist.new options
     def initialize options
       set_traps_for_signals
       @options = options.dup
@@ -150,22 +185,41 @@ module Spfy
       parse @options
     end
 
-    attr_reader :creator, :title, :options, :paths
+
+    # @see http://xspf.org/xspf-v1.html#rfc.section.4.1.1.2.2
+    # @return [String]
+    attr_reader :creator
+
+    # @see http://xspf.org/xspf-v1.html#rfc.section.4.1.1.2.2
+    # @return [String]
+    attr_reader :title
+
+
+    # @return [Hash] The options hash passed in.
+    attr_reader :options
+
+    # The paths that will be searched for files.
+    # @return [<Pathname>]
+    attr_reader :paths
+
 
     # @see http://xspf.org/xspf-v1.html#rfc.section.4.1.1.2.3
+    # @api public
+    # @return [String]
     def annotation
       @annotation
     end
 
 
     # @see http://xspf.org/xspf-v1.html#rfc.section.4.1.1.2.8
+    # @api public
+    # @return [String] Time now formatted as an ISO8601 date.
     def date
       Time.now.iso8601
     end
 
 
-    # @private
-    # The option parser
+    # Parse the options further to set needed instance variables.
     # spfy here there and everywhere
     # {
     #   "--help"        =>  false,
@@ -183,6 +237,8 @@ module Spfy
     #   "PATHS"=>["spec/support/fixtures/albums/mp4/mp4.m4a"],
     #   "--version"     =>  false
     # }
+    # @api private
+    # @return [Hash] Ignore the return value, this is a side-effect, it sets several instance variables
     def parse options
       @paths = (options.delete("PATHS") || []).map{|path| Pathname(path.sub /^~/, ENV["HOME"]) }
       return if @paths.empty?
@@ -198,11 +254,13 @@ module Spfy
       @creator = options.delete("--creator") || ENV["USER"]
       @annotation = options.delete("--annotation") || "Created with Spfy.rb"
       @max_tracks = options["--max-tracks"] && options["--max-tracks"].to_i
+      options
     end
 
 
-    # @private
     # For interruptions
+    # @api private
+    # @return There is no return value, it's a side-effect
     def set_traps_for_signals
       trap(:SIGINT) do
         warn "  Received Ctrl+c"
@@ -213,6 +271,10 @@ module Spfy
 
 
     # Render the playlist and any tracks.
+    # @api public
+    # @example
+    #   playlist.to_xml
+    # @return [String] XML output
     def to_xml
       return "" if @paths.empty?
       mapped = []
